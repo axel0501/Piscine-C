c8e7e9d136465cfbedb1590f3d9b58ef0a63c3e1
Alessandro Metta <ametta@e3r4p7.42roma.it>
2020-11-27 12:58:12 +0100

    day_4_v0.2

---
+++ _progetti/c_00/ex01/ft_print_alphabet.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ametta <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/26 10:31:15 by ametta            #+#    #+#             */
+/*   Updated: 2020/11/26 10:53:01 by ametta           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_alphabet(void)
+{
+	char letter;
+
+	letter = 'a';
+	while (letter <= 'z')
+	{
+		ft_putchar(letter);
+		letter++;
+	}
+}
