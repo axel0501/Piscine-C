35ea2e8a8eac3f5e70a01781821b8eb61b4c07fe
Alessandro Metta <ametta@e4r4p3.42roma.it>
2020-11-29 20:16:43 +0100

    day_6

--- Rush00/rush03.c
+++ Rush00/rush03.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   rush04.c                                           :+:      :+:    :+:   */
+/*   rush03.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dlanotte <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/28 16:42:10 by dlanotte          #+#    #+#             */
-/*   Updated: 2020/11/28 18:10:12 by dlanotte         ###   ########.fr       */
+/*   Created: 2020/11/29 19:43:43 by dlanotte          #+#    #+#             */
+/*   Updated: 2020/11/29 19:44:35 by dlanotte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,17 @@ void	ft_putchar(char c);
 
 void	ft_corners(int x, int y, int c_y, int c_x)
 {
-	if (c_x == 1 && c_y == 1)
+	if (c_x == 0 && c_y == 0)
 		ft_putchar('A');
-	else if (c_x == 1 && c_y == y)
+	else if (c_x == 0 && c_y == y)
 		ft_putchar('A');
-	else if (c_x == x && c_y == 1)
+	else if (c_x == x && c_y == 0)
 		ft_putchar('C');
 	else if (c_x == x && c_y == y)
 		ft_putchar('C');
-	else if (c_y == 1 || c_y == y)
+	else if (c_y == 0 || c_y == y)
 		ft_putchar('B');
-	else if (c_x == 1 || c_x == x)
+	else if (c_x == 0 || c_x == x)
 		ft_putchar('B');
 	else
 		ft_putchar(' ');
@@ -35,11 +35,11 @@ void	ft_box(int x, int y)
 	int c_x;
 	int c_y;
 
-	c_x = 1;
-	c_y = 1;
+	c_x = 0;
+	c_y = 0;
 	while (c_y <= y)
 	{
-		c_x = 1;
+		c_x = 0;
 		while (c_x <= x)
 		{
 			ft_corners(x, y, c_y, c_x);
@@ -54,6 +54,6 @@ void	rush(int x, int y)
 {
 	if (x > 0 && y > 0)
 	{
-		ft_box(x, y);
+		ft_box(x - 1, y - 1);
 	}
 }
--- _progetti/c01/main.c
+++ _progetti/c01/main.c
@@ -3,17 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pbarbaro <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: ametta <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/28 12:10:26 by pbarbaro          #+#    #+#             */
-/*   Updated: 2020/11/29 19:30:56 by dlanotte         ###   ########.fr       */
+/*   Created: 2020/11/29 17:03:56 by ametta            #+#    #+#             */
+/*   Updated: 2020/11/29 18:40:32 by ametta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	rush(int x, int y);
+#include <unistd.h>
 
-int		main(void)
+void		ft_putchar(char c)
 {
-	rush(5, 3);
-	return (0);
+	write(1, &c, 1);
+}
+
+void		ft_swap(int *a, int *b);
+
+int			main(void)
+{
+	int a;
+	int b;
+
+	a = 5;
+	b = 0;
+	ft_swap(&a, &b);
+	ft_putchar(a + 48);
+	ft_putchar('\n');
+	ft_putchar(b + 48);
+	ft_putchar('\n');
 }
